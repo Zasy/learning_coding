70d8798ea8399c571f098b02f8af7c6360571758
markzzhang <markzzhang@tencent.com>
2018-09-09 09:18:58 +0800

    add the cplusplus example

---
+++ CLionProjects/newProject/example.cpp
@@ -0,0 +1,35 @@
+////
+//// Created by 张哲 on 2018/9/9.
+////
+//
+//#include <vector>
+//#include <deque>          // std::deque
+//#include <list>           // std::list
+//#include <queue>          // std::queue
+//#include <iostream>
+//#include <algorithm>
+//#include <map>
+//#include <string>
+//using namespace std;
+//
+//struct Data {
+//    int age;
+//    string name;
+//};
+//
+//int main() {
+//    int N,M;
+//    cin >> N >> M;
+//    vector<Data> test;
+//    for(int i = 0; i < N ; i++){
+//        Data test_temp;
+//        cin >> test_temp.age >> test_temp.name;
+//        test.push_back(test_temp);
+//    }
+//
+//
+//    for(int i = 0; i < N; i++) {
+//        cout << test[i].age << test[i].name << endl;
+//    }
+//}
+//
